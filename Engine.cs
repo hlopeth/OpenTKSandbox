c83b22672c0dde009f88ccd4b9ba1692084e9754
Nikita <mnv.rainman@gmail.com>
2018-05-07 11:00:25 +0500

    Initial commit

---
+++ Engine.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Drawing;
+using OpenTK;
+using OpenTK.Graphics.OpenGL;
+using OpenTK.Input;
+
+namespace OpenTKSandbox
+{
+    public class Engine
+    {
+        private readonly GameWindow _window;
+
+        public Engine(GameWindow window)
+        {
+            _window = window;
+            _window.Load += WindowOnLoad;
+            _window.Resize +=WindowOnResize;
+            _window.UpdateFrame += WindowOnUpdateFrame;
+            _window.RenderFrame += WindowOnRenderFrame;
+            _window.KeyDown += WindowOnKeyDown;
+        }
+
+        private void WindowOnLoad(object sender, EventArgs e)
+        {
+            //GL.Enable(EnableCap.DepthTest);
+        }
+
+        private void WindowOnResize(object sender, EventArgs e)
+        {
+            GL.Viewport(0, 0, _window.Width, _window.Height);
+        }
+
+        private void WindowOnUpdateFrame(object sender, FrameEventArgs e)
+        {
+            
+        }
+
+        private void WindowOnRenderFrame(object sender, FrameEventArgs e)
+        {
+            GL.ClearColor(Color.Gray);
+            GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+            
+            _window.SwapBuffers();
+        }
+
+        private void WindowOnKeyDown(object sender, KeyboardKeyEventArgs e)
+        {
+            switch (e.Key)
+            {
+                case Key.Escape:
+                    _window.Close();
+                    break;
+                default:
+                    Console.WriteLine($"no {e.Key} key handler");
+                    break;
+            }   
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using OpenTK;
+using OpenTK.Graphics;
+
+namespace OpenTKSandbox
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var window = new GameWindow(800, 600, GraphicsMode.Default, "Sandbox");
+            var engine = new Engine(window);
+            window.Run();
+        }
+    }
+}
